9220bb1c4d85a0e1406c0ef693364b8b24fa7c86
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2016-08-09 19:49:51 +0200

    Device detector extended

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -18,6 +18,14 @@ class DeviceController extends Controller {
             'browser' => $agent->browser->toString(),
             'engine' => $agent->engine->toString(),
             'os' => $agent->os->toString(),
+            'isCellphone' => $agent->isMobile(),
+            'isTablet' => $agent->isType('tablet'),
+            'isRobot' => $agent->isType('bot'),
+            'isEreader' => $agent->isType('ereader'),
+            'isDesktop' => $agent->isType('monitor'),
+            'isPos' => $agent->isType('pos'),
+            'isGaming' => $agent->isType('gaming'),
+            'isEmulator' => $agent->isType('emulator'),
         );
         
         return response()->json($data, 200, [], JSON_PRETTY_PRINT);
